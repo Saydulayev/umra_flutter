46b32ff8ebcd6be3bfaa99a4a20c3122bf0e4392
Saydulayev <saydulayev.wien@gmail.com>
2025-11-17 14:38:27 +0100

    Refactor Android project structure and update versioning
    - Updated version in pubspec.yaml to 1.0.0+1 for a new release.
    - Modified AndroidManifest.xml to change app label to "UMRA".
    - Updated build.gradle.kts to load keystore properties for signing configurations.
    - Changed application ID and namespace to "saydulayev.wien_gmail.com.umra".
    - Added MainActivity.kt for the new package structure.
    - Updated .gitignore to include keystore files.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +7,15 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// Load keystore properties
+val keystoreProperties = Properties()
+val keystorePropertiesFile = file("../key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystorePropertiesFile.inputStream().use { keystoreProperties.load(it) }
+}
+
 android {
-    namespace = "com.example.umra_flutter"
+    namespace = "saydulayev.wien_gmail.com.umra"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.umra_flutter"
+        applicationId = "saydulayev.wien_gmail.com.umra"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -31,11 +40,19 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            val storeFileProp = keystoreProperties["storeFile"] as String?
+            storeFile = storeFileProp?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/saydulayev/wien_gmail/com/umra/MainActivity.kt
+++ android/app/src/main/kotlin/saydulayev/wien_gmail/com/umra/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.umra_flutter
+package saydulayev.wien_gmail.com.umra
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+
